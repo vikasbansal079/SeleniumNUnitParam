26e30cd9e3c1a069b550d3e3ca3c2c6f0adef362
Vikas Bansal <vikasbansal079@gmail.com>
2020-04-23 15:14:55 +0530

    Added thread sleep

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -10,37 +10,18 @@ namespace SeleniumNUnitParam
 {
     public class BrowserTest : Hooks
     {
-
-
-        [Test]
-        public void GoogleTest()
-        {
-            Driver.Navigate().GoToUrl("http://www.google.com");
-            System.Threading.Thread.Sleep(50000);
-            Driver.FindElement(By.Name("q")).SendKeys("Selenium");
-            System.Threading.Thread.Sleep(50000);
-            Driver.FindElement(By.Name("btnG")).Click();
-            Assert.True(true);
-            //Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-            //                                "The text selenium doest not exist");
-
-        }
-
         [Test]
         public void ExecuteAutomationTest()
         {
             Driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
-            System.Threading.Thread.Sleep(50000);
+            System.Threading.Thread.Sleep(5000);
             Driver.FindElement(By.Name("UserName")).SendKeys("admin");
             Driver.FindElement(By.Name("Password")).SendKeys("admin");
             Driver.FindElement(By.Name("Login")).Submit();
-            System.Threading.Thread.Sleep(20000);
-            Assert.True(true);
-            //Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-            //                                "The text selenium doest not exist");
+            System.Threading.Thread.Sleep(5000);
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                            "The text selenium doest not exist");
 
         }
-
-
     }
 }
